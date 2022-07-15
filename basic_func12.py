f2652bb9f85fdece523207c9027cbf593bfa1875
Jalilov <javohirjalilov777@gmail.com>
2022-07-15 06:15:25 +0500

    added tasks

---
+++ basic_func12.py
@@ -0,0 +1,4 @@
+# Create a function called main.
+# Create function argument a.
+# Round the result to 2 decimal places.
+# Return the value of a.
---
+++ basic_func18.py
@@ -0,0 +1,4 @@
+# Create a function called main.
+# Create function arguments a.
+# Round the result to 2 decimal places.
+# Assign the value pi to the variable "a" and return.
